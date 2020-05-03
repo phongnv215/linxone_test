3b7f5fc1c1a00277fcf0c15767d4b1eb9e91d3e4
phongnv <phongnv2105@gmail.com>
2020-05-03 16:27:40 +0700

    Test construct project push to visual

---
+++ LinxOne_testConnection/LinxOne_testConnection/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LinxOne_testConnection.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
